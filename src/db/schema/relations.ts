86d389f3b09584e834efc82107df146e28992ade
Bintang Fajar Putra Pamungkas <bintangfajarputra@gmail.com>
2025-12-22 22:46:32 +0700

    [style]: fix formatting

--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -3,7 +3,6 @@ import { relations } from "drizzle-orm";
 import { foundItems } from "@/db/schema/found-items";
 import { claimRequests } from "@/db/schema/claim-requests";
 
-
 export const foundItemsRelations = relations(foundItems, ({ many }) => ({
 	claims: many(claimRequests),
 }));
